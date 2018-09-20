8a2fba0f2d404659d49a8dfe7352861e52e972f5
Martin Alejandro Liut <martin.liut@globant.com>
2018-09-20 10:49:14 -0300

    Working on footer II

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import image from './../img/martin.png';
 import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 
 const styleAvatar = {
     top: '-45px',
-    margin: 'auto',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    position: 'absolute',
     height: '100px',
     width: '100px'
 }
@@ -17,12 +20,12 @@ const Footer = () => (
     <footer>
         <Grid container spacing={16} justify="center">
             <Grid item xs={2}>
-                <Card style={{position: "relative", overflow:"visible", textAlign: 'center'}}>
-                    <Avatar alt="Martin Liut" src={image} id="avatar" style={styleAvatar} />
-                    <CardContent style={{}}>
-                        
-                        <Typography component="h1" variant="title">By Martin Liut</Typography>
-                        <Typography component="p" variant="subheading">
+                <Card style={{overflow:"visible", textAlign: 'center'}}>
+                    
+                    <CardContent style={{position: "relative"}}>
+                        <Avatar alt="Martin Liut" src={image} id="avatar" style={styleAvatar} />
+                        <Typography component="h1" variant="title" className="martin">Martin Liut</Typography>
+                        <Typography component="p" variant="subheading" className="p">
                             WEB UI Developer
                         </Typography>
                     </CardContent>
